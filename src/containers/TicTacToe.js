dacda9bf0df8a00236eadfb7fa149a1458fcde56
YOUR_GITHUB_USERNAMEcd <fevillarosa73@gmail.com>
2018-07-12 15:49:01 -0700

    containers updated

---
+++ src/containers/TicTacToe.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+
+class TicTacToe extends Component {
+
+    state = {
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    move = () => {
+
+    }
+
+    makeAiMove = () => {
+
+    }
+
+    ticTacToeTest = () => {
+
+    }
+
+    recordGame = () => {
+
+    } 
+
+    render() {
+        return (
+            <div>
+              {/* */} 
+            </div>
+        )
+    }
+}
+
+record 
+
+
+
+
+export default TicTacToe
--- src/styled/Template.js
+++ src/styled/Template.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import {media} from '../utils/media'
 
 export const Header = styled.header`
     text-align: center;
@@ -9,10 +10,13 @@ export const Header = styled.header`
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    align-items:
+    align-items: center;
     margin: auto;
     width: 80%;
     min-height: 80vh;
+    ${media.handheld`
+        width: 100%;
+    `}
 `
 
 export const Main = (props) => {
