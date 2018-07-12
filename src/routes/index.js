175e306c28f356091ae2340d601dcefa75d19704
YOUR_GITHUB_USERNAMEcd <fevillarosa73@gmail.com>
2018-07-12 09:37:49 -0700

    first heroku commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import {Route, indexRoute} from 'react-router'
-
+import {Route, IndexRoute} from 'react-router'
+import Template from '../containers/Template'
+import Home from '../containers/Home'
+import Profile from '../containers/Profile'
 
 const createRoutes = () => {
     return (
         <Route
             path='/'
             component={Template}
-
         >
+        <IndexRoute
+            path='/'
+            component={Home}
+        />
+
+        <Route
+            path={'/profile'}
+            component={Profile}
+        />
 
         </Route>
     )
